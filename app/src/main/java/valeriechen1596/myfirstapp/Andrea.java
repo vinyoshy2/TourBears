5aa41fa45d545e17f2ddb41b70a20c83dfa65960
Valerie <vchen2@berkeley.edu>
2016-06-10 20:10:01 -0700

    initial commit

---
+++ app/src/main/java/valeriechen1596/myfirstapp/Andrea.java
@@ -0,0 +1,13 @@
+package valeriechen1596.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Andrea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_andrea);
+    }
+}
